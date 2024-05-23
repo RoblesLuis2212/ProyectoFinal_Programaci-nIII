9813a2e42ded228765a8c5df66a3903e30c54a95
RoblesLuis2212 <luisgeremiasrobles@gmail.com>
2024-05-23 00:04:30 -0300

    se agregaron mas menu y se limpia la consola

--- ProyectoConsola.py
+++ ProyectoConsola.py
@@ -11,6 +11,11 @@ def CrearArchivos():
     ArchivoPedidos = open("Direccion.txt","w")
     ArchivoID = open("IdClientes.txt","w")
 
+
+def LimpiarConsola():
+    os.system('cls')
+
+
 #Funcion Registrarse
 def Registro():
     print("------Registro-----")
@@ -27,6 +32,8 @@ def Registro():
     LeerGerente.close()
     LeerContraseñas.close()
     print("Usuario generado correctamente,Gracias por elegirnos")
+    time.sleep(3)
+    LimpiarConsola()
     MenuClientes()
 
 #Esta funcion permitira al sistema poder identificar que tipo de usuario esta accediendo
@@ -41,15 +48,23 @@ def LoginClientes():
     ListaClientes = AbrirClientes.readlines()
     ListaContraseña = AbrirContraseñas.readlines()
 
-    if Usuario in ListaClientes and Contraseña in ListaContraseña:
-        print("Cliente encontrado en la lista")
+    if Usuario + '\n' in ListaClientes:
+        indice = ListaClientes.index(Usuario + '\n')
+        if ListaContraseña[indice] == Contraseña + '\n':
+            print("Cliente encontrado en la lista")
+            LimpiarConsola()
+            MenuComidas()
+        else:
+            print("Contraseña incorrecta")
     else:
-        print("Cliente inexistente") #Mostramos error si el usuario o contraseña no son encontrados
+        print("Cliente inexistente")
 
 def Instalador():
     if path.exists("Gerente.txt"):
         print("Archivos encontrados accediendo al sistema...")
-        MenuClientes()
+        time.sleep(2)
+        LimpiarConsola()
+        MenuInicio()
     else:
         print("Archivo inexistentes...")
         print("Creando Archivos...")
@@ -68,11 +83,39 @@ def MenuClientes():
     opcion = int(input("Seleccione una opcion: "))
 
     if opcion == 1:
+        LimpiarConsola()
         Registro()
     elif opcion == 2:
+        LimpiarConsola()
         LoginClientes()
 
 
+def MenuInicio():
+    print("----SISTEMA GASTRONOMICO-----")
+    print("1.Iniciar Sesion")
+    print("2.Registrarse")
+    print("3.Salir")
+    opcion = int(input("Seleccione una opcion: "))
+
+    if opcion == 1:
+        LimpiarConsola()
+        LoginClientes()
+    elif opcion == 2:
+        LimpiarConsola()
+        Registro()
+    elif opcion == 3:
+        LimpiarConsola()
+        print("Hasta luego vuelva pronto")
+        exit()
+
+
+def MenuComidas():
+    print("--------Menu De Comidas------")
+    print("1.Fideo Tallerin con salsa")
+    print("2.Hamburguesa Especial")
+    print("3.Canelones de verdura")
+    print("Milanesa Napolitana con pure")
+    opcion = input("Seleccione una opcion: ")
 
 
 
